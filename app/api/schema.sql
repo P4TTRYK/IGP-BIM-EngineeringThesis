e17b5ab5f3372a70f917d53cd898b45398b0c727
fyrtax <33764198+fyrtax@users.noreply.github.com>
2025-11-13 18:48:58 +0100

    Add triggers for updated_at columns

--- app/api/schema.sql
+++ app/api/schema.sql
@@ -1,19 +1,32 @@
-create table if not exists projects (
-    id integer primary key autoincrement,
-    name text not null,
+create table if not exists projects
+(
+    id          integer primary key autoincrement,
+    name        text not null,
     description text,
-    created_at datetime default current_timestamp,
-    updated_at datetime default current_timestamp
+    created_at  datetime default current_timestamp,
+    updated_at  datetime default current_timestamp
 );
 
-create table if not exists survey (
-    id integer primary key autoincrement,
+create table if not exists survey
+(
+    id         integer primary key autoincrement,
     project_id integer not null,
-    metadata text,
-    photos text,
+    metadata   text,
+    photos     text,
     updated_at datetime default current_timestamp,
-    foreign key (project_id) references projects(id) on delete cascade
+    foreign key (project_id) references projects (id) on delete cascade
 );
 
--- TODO: Autoupdate 'updated_at' on row update
--- TODO: Fix bug
+create trigger update_project_updated_at
+    after update
+    on projects
+begin
+    update projects set updated_at = current_timestamp where id = old.id;
+end;
+
+create trigger update_survey_updated_at
+    after update
+    on survey
+begin
+    update survey set updated_at = current_timestamp where id = old.id;
+end;
