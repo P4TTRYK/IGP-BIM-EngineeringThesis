bc65bc9e0a346b50bb9eca9c8cc71cf9eeaeb92f
fyrtax <33764198+fyrtax@users.noreply.github.com>
2025-11-21 23:52:53 +0100

    Add top nav info bar to xeo project viewer

--- app/xeokit/src/pages/Project.jsx
+++ app/xeokit/src/pages/Project.jsx
@@ -1,16 +1,36 @@
-import {useParams} from "react-router";
+import {Link, useParams} from "react-router";
 import {Xeokit} from "../components/Xeokit.jsx";
-import {useProjectModelQuery} from "../services/api.js";
+import {useProjectListQuery, useProjectModelQuery} from "../services/api.js";
+import styles from "./Project.module.css";
 
 export const Project = () => {
     const {projectId} = useParams();
-    const {data: model, isFetching, error} = useProjectModelQuery(projectId);
+    const {data: model, isFetching: fetchingModel, error: errorModel} = useProjectModelQuery(projectId);
+    const {data: projectsList} = useProjectListQuery();
+
+    let projectInfo = {};
+    if (projectsList && projectsList.length > 0) {
+        projectInfo = projectsList.find(proj => proj.id.toString() === projectId);
+    }
 
     return (
-        <>
-            {isFetching && <div>Loading model...</div>}
-            {error && <div>Error loading model: {error.status}</div>}
-            {!isFetching && !error && model && <Xeokit model={model}/>}
-        </>
+        <div className={styles.projectContainer}>
+            <nav className={styles['top-bar']}>
+                <Link to={"/"}>
+                    <button className={styles['back-button']}>&lt; Lista projektów</button>
+                </Link>
+                <h3>Projekt: {projectInfo.name ?? '...'}</h3>
+                <div className={styles['model-info']}>
+                    <span>{projectInfo.guid ?? '...'}</span>
+                    <span>{projectInfo.description ?? ''}</span>
+                </div>
+            </nav>
+
+            <div className={styles['xeokit-container']}>
+                {fetchingModel && <div>Ładowanie modelu...</div>}
+                {errorModel && <div className={styles['error-text']}>Wystąpił błąd: {errorModel.status}</div>}
+                {!fetchingModel && !errorModel && model && <Xeokit model={model}/>}
+            </div>
+        </div>
     );
 }
