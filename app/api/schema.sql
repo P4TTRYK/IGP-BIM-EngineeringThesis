b86561ead4f79cff90ad0540ef9ebf381896a644
fyrtax <33764198+fyrtax@users.noreply.github.com>
2025-11-25 13:46:35 +0100

    Update photos table

--- app/api/schema.sql
+++ app/api/schema.sql
@@ -8,24 +8,23 @@ create table if not exists projects
     updated_at  datetime default current_timestamp
 );
 
-create table if not exists survey
-(
-    id         integer primary key autoincrement,
-    guid       text    not null,
-    project_id integer not null,
-    metadata   text,
-    photos     text,
-    updated_at datetime default current_timestamp,
-    unique (guid, project_id),
-    foreign key (project_id) references projects (id) on delete cascade
-);
-
 create trigger if not exists update_project_updated_at
     after update
     on projects
 begin
     update projects set updated_at = current_timestamp where id = old.id;
 end;
+
+create table if not exists survey
+(
+    id         integer primary key autoincrement,
+    guid       text    not null,
+    project_id integer not null,
+    metadata   text,
+    updated_at datetime default current_timestamp,
+    unique (guid, project_id),
+    foreign key (project_id) references projects (id) on delete cascade
+);
 
 create trigger if not exists update_survey_updated_at
     after update
@@ -37,25 +36,20 @@ end;
 
 create table if not exists photos
 (
-    id          integer primary key autoincrement,
-    guid        text    not null unique,
-    survey_id   integer not null,
-    project_id  integer not null,
-    filename    text    not null,
-    filepath    text    not null,
-    file_size   integer,
-    mime_type   text,
-    description text,
-    created_at  datetime default current_timestamp,
-    updated_at  datetime default current_timestamp,
-    foreign key (survey_id) references survey (id) on delete cascade,
-    foreign key (project_id) references projects (id) on delete cascade
+    id         integer primary key autoincrement,
+    survey_id  integer not null,
+    filename   text    not null,
+    deleted    boolean  default false,
+    created_at datetime default current_timestamp,
+    foreign key (survey_id) references survey (id) on delete cascade
 );
 
 create trigger if not exists update_photos_updated_at
     after update
     on photos
 begin
-    update photos set updated_at = current_timestamp where id = new.id;
-    update projects set updated_at = current_timestamp where id = new.project_id;
+    update survey set updated_at = current_timestamp where id = old.survey_id;
+    update projects
+    set updated_at = current_timestamp
+    where id = (select project_id from survey where id = old.survey_id);
 end;
