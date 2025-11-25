b253e797f8a25475db2bef1ebd268c77e2abf0c3
fyrtax <33764198+fyrtax@users.noreply.github.com>
2025-11-25 14:26:34 +0100

    Update get_proj_changes sql to new schema

--- app/api/database/get_proj_changes.py
+++ app/api/database/get_proj_changes.py
@@ -4,9 +4,14 @@ import sqlite3
 def get_proj_changes(cursor, project_id):
     try:
         cursor.execute("""
-                       SELECT guid, metadata, photos, updated_at
-                       FROM survey
-                       WHERE project_id = ?
+                       SELECT s.guid,
+                              s.metadata,
+                              json_group_array(p.filename) as photos,
+                              s.updated_at
+                       FROM survey s
+                                LEFT JOIN photos p ON p.survey_id = s.id AND (p.deleted IS NULL OR p.deleted = 0)
+                       WHERE s.project_id = ?
+                       GROUP BY 1, 2, 4;
                        """, (project_id,))
         rows = cursor.fetchall()
 
