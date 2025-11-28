6ac8dfffc00514946b88d9f1d729f268b8764b40
fyrtax <33764198+fyrtax@users.noreply.github.com>
2025-11-28 23:39:26 +0100

    Add footer

---
+++ app/xeokit/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+
+export const Footer = () => {
+    return (
+        <footer
+            className={styles.footer}
+        >
+            &copy; 2025 Projekt Bim Scope
+        </footer>
+    )
+}
--- app/xeokit/src/pages/ProjectsList.jsx
+++ app/xeokit/src/pages/ProjectsList.jsx
@@ -2,30 +2,34 @@ import {UploadIfcFile} from "../components/UploadIfcFile.jsx";
 import {ProjectCard} from "../components/Project_card.jsx";
 import {useProjectListQuery} from "../services/api.js";
 import styles from './ProjectsList.module.css';
+import {Footer} from "../components/Footer.jsx";
 
 export const ProjectsList = () => {
     const {data, isLoading, error, refetch} = useProjectListQuery();
     return (
-        <div className={styles['projects-list-page']}>
-            <h2>Utwórz nowy projekt przesyłając plik IFC</h2>
-            <UploadIfcFile onUpload={refetch}/>
+        <>
+            <div className={styles['projects-list-page']}>
+                <h2>Utwórz nowy projekt przesyłając plik IFC</h2>
+                <UploadIfcFile onUpload={refetch}/>
 
-            <h1>Lista projektów</h1>
-            <div className={styles['project-list']}>
-                {isLoading && <h3>Ładowanie projektów...</h3>}
-                {error && <h3 className={styles['error-text']}>Wystąpił błąd: {error.status}</h3>}
-                {!isLoading && !error && (
-                    (!data || data.length === 0)
-                        ? <h3>Brak projektów</h3>
-                        : data.map((project, idx) =>
-                            <ProjectCard
-                                key={idx}
-                                project={project}
-                                style={{animationDelay: `${idx * 50}ms`}}
-                            />
-                        )
-                )}
+                <h1>Lista projektów</h1>
+                <div className={styles['project-list']}>
+                    {isLoading && <h3>Ładowanie projektów...</h3>}
+                    {error && <h3 className={styles['error-text']}>Wystąpił błąd: {error.status}</h3>}
+                    {!isLoading && !error && (
+                        (!data || data.length === 0)
+                            ? <h3>Brak projektów</h3>
+                            : data.map((project, idx) =>
+                                <ProjectCard
+                                    key={idx}
+                                    project={project}
+                                    style={{animationDelay: `${idx * 50}ms`}}
+                                />
+                            )
+                    )}
+                </div>
             </div>
-        </div>
+            <Footer/>
+        </>
     )
 }
