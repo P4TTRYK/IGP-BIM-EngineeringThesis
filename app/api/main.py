7fa43474e50ea577b4a518722dc60a03d0607e28
fyrtax <33764198+fyrtax@users.noreply.github.com>
2025-11-28 22:57:04 +0100

    Specify project_id in api request is integer

--- app/api/main.py
+++ app/api/main.py
@@ -29,11 +29,11 @@ def get_projects():
     return jsonify(projects), code
 
 
-@app.get('/project/<project_id>/image')
+@app.get('/project/<int:project_id>/image')
 def get_project_image(project_id):
     return send_from_directory(
         f"./uploads",
-        f"{project_id}.png",
+        f"{str(project_id)}.png",
         as_attachment=False
     )
 
@@ -41,16 +41,16 @@ def get_project_image(project_id):
 @app.get('/project/<project_id>/changes')
 def get_project_changes(project_id):
     db = DB()
-    projects, code = get_proj_changes(db.cursor, project_id)
+    changes, code = get_proj_changes(db.cursor, str(project_id))
     db.connection.close()
 
-    return jsonify(projects), code
+    return jsonify(changes), code
 
 
-@app.post('/project/<project_id>/changes')
+@app.post('/project/<int:project_id>/changes')
 def save_project_changes(project_id):
     db = DB()
-    result, code = save_proj_changes(db.cursor, project_id, request.form)
+    result, code = save_proj_changes(db.cursor, str(project_id), request.form)
     db.connection.commit()
     db.connection.close()
 
@@ -93,7 +93,7 @@ def get_xkt(project_id):
     )
 
 
-@app.get('/project/<project_id>/survey/<guid>/image/<image_id>')
+@app.get('/project/<int:project_id>/survey/<guid>/image/<image_id>')
 def get_survey_image(project_id, guid, image_id):
     # https://developer.mozilla.org/en-US/docs/Web/Media/Guides/Formats/Image_types
     if not any(image_id.endswith(ext) for ext in ACCEPTED_IMAGE_EXTENSIONS):
@@ -101,24 +101,24 @@ def get_survey_image(project_id, guid, image_id):
 
     return send_from_directory(
         f"./uploads/photos",
-        f"{project_id}_{guid}_{image_id}",
+        f"{str(project_id)}_{guid}_{image_id}",
         as_attachment=False
     )
 
 
-@app.get('/project/<project_id>/survey/<guid>/image/<image_id>/small')
+@app.get('/project/<int:project_id>/survey/<guid>/image/<image_id>/small')
 def get_survey_image_thumbnail(project_id, guid, image_id):
     if not any(image_id.endswith(ext) for ext in ACCEPTED_IMAGE_EXTENSIONS):
         return INVALID_FILE_RESPONSE
 
     return send_from_directory(
         f"./uploads/photos",
-        f"small_{project_id}_{guid}_{image_id}",
+        f"small_{str(project_id)}_{guid}_{image_id}",
         as_attachment=False
     )
 
 
-@app.post('/project/<project_id>/survey/<guid>/image')
+@app.post('/project/<int:project_id>/survey/<guid>/image')
 def post_survey_image(project_id, guid):
     if 'file' not in request.files:
         return "No file has been provided", 400
@@ -150,7 +150,7 @@ def post_survey_image(project_id, guid):
         cursor=db.cursor,
         file=file,
         file_format=file_format,
-        project_id=project_id,
+        project_id=str(project_id),
         guid=guid
     )
     db.connection.commit()
