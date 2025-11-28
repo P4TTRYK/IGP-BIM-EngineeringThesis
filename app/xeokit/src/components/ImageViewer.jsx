36908f87385999b4172fcc28b24db0aeb3daf630
fyrtax <33764198+fyrtax@users.noreply.github.com>
2025-11-29 00:30:47 +0100

    Add survey image viewer

---
+++ app/xeokit/src/components/ImageViewer.jsx
@@ -0,0 +1,26 @@
+import styles from './ImageViewer.module.css';
+import {Icon} from "./Icon.jsx";
+
+export const ImageViewer = ({visible, imageLink, onHide}) => {
+    console.log(imageLink);
+    return (
+        <>
+            <div
+                className={`${styles.background} ${visible ? styles.visible : ''}`}
+                onClick={onHide}
+            >
+                {visible &&
+                    <img
+                        className={styles.image}
+                        onClick={e => e.stopPropagation()}
+                        src={imageLink} alt="Image"
+                    />
+                }
+
+                <div className={styles.close}>
+                    <Icon.close/>
+                </div>
+            </div>
+        </>
+    )
+}
