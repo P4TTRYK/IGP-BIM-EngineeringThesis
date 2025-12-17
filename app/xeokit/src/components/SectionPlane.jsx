b7e75629ba7fce6cbfc866cd99247a9c7889bbd7
fyrtax <33764198+fyrtax@users.noreply.github.com>
2025-12-17 17:54:17 +0100

    Refactor SectionPlane

--- app/xeokit/src/components/SectionPlane.jsx
+++ app/xeokit/src/components/SectionPlane.jsx
@@ -1,49 +1,43 @@
 // https://xeokit.github.io/xeokit-sdk/examples/slicing/#SectionPlanesPlugin_Duplex_FastNavPlugin
-import {useEffect, useRef, useState} from "react";
+import {useEffect, useRef} from "react";
 import {math, SectionPlanesPlugin} from "@xeokit/xeokit-sdk";
 
 export function SectionPlane({viewer, enabled}) {
     const sectionPlanesRef = useRef(null);
-    const sectionPlaneRef = useRef(null);
-    const [sectionId, setSectionId] = useState(null);
 
     useEffect(() => {
         if (!viewer) return;
 
         const sectionPlanes = new SectionPlanesPlugin(viewer, {
-            overviewCanvasId: "mySectionPlanesOverviewCanvas",
-            overviewVisible: false
-        });
-
-        const sectionPlane = sectionPlanes.createSectionPlane({
-            id: "mySectionPlane",
-            pos: viewer.scene.center,
-            dir: math.normalizeVec3([1.0, 0.01, -1.0])
+            overviewCanvasId: "mySectionPlanesOverviewCanvas"
         });
 
         sectionPlanesRef.current = sectionPlanes;
-        sectionPlaneRef.current = sectionPlane;
 
-        setSectionId(sectionPlane.id);
         return () => {
             sectionPlanes.clear();
         };
     }, [viewer]);
 
     useEffect(() => {
-        if (sectionPlaneRef.current && sectionPlanesRef.current && sectionId) {
+        const sectionPlanes = sectionPlanesRef.current;
+        if (sectionPlanes && viewer) {
             if (enabled) {
-                console.log(sectionId);
-                sectionPlaneRef.current.active = true;
-                sectionPlanesRef.current.showControl("mySectionPlane");
-                sectionPlanesRef.current.setOverviewVisible(true);
+                sectionPlanes.createSectionPlane({
+                    id: "mySectionPlane",
+                    pos: viewer.scene.center,
+                    dir: math.normalizeVec3([1.0, 0.01, -1.0])
+                });
+
+                sectionPlanes.showControl("mySectionPlane");
+                sectionPlanes.setOverviewVisible(true);
             } else {
-                sectionPlaneRef.current.active = false;
-                sectionPlanesRef.current.hideControl();
-                sectionPlanesRef.current.setOverviewVisible(false);
+                sectionPlanes.clear();
+                sectionPlanes.hideControl();
+                sectionPlanes.setOverviewVisible(false);
             }
         }
-    }, [enabled, sectionId]);
+    }, [enabled, viewer]);
 
     return null;
 }
